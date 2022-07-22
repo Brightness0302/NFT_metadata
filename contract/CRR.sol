d2e7d6f5ed032dae6fb818039f4c509e884aadf3
Jacky Smith <JackySmithH2002@gmail.com>
2022-07-22 01:53:21 -0400

    image & metadata

---
+++ contract/CRR.sol
@@ -0,0 +1,77 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+import "./ERC721A.sol";
+
+contract CRR is
+    ERC721A,
+    Ownable,
+    ReentrancyGuard,
+{
+    using ECDSA for bytes32;
+
+    uint256 public constant MAX_SUPPLY = 5000;
+
+    uint256 public salePrice = 0.1 ether;
+
+    bool private hasOwnerMint;
+
+    constructor() ERC721A("Crazy Rich Rabbit", "CRR") {}
+
+    modifier onlyEOA() {
+        require(tx.origin == msg.sender, "Contracts not allowed");
+        _;
+    }
+
+    //  Set Sale Price
+    function setSalePrice(uint256 price) external onlyOwner {
+        salePrice = price;
+    }
+
+    //  owner mint
+    function OwnerMint() external payable onlyOwner {
+        require(hasOwnerMint != true, "Already Owner Mint");
+        _safeMint(msg.sender, 1);
+        hasOwnerMint = true;
+    }
+
+    // Public Sale
+    function PublicSalesMint(uint256 qty)
+        external
+        payable
+        nonReentrant
+        onlyEOA
+    {
+        require(qty != 0, "Public Sale: No Quantity");
+        require(
+            totalSupply() + qty <= MAX_SUPPLY,
+            "Public Sale: Over Max Supply"
+        );
+        require(msg.value >= salePrice * qty, "Public Sale: Insufficient ETH");
+
+        _safeMint(msg.sender, qty);
+    }
+
+    // // metadata URI
+    string private baseTokenURI;
+
+    function _baseURI() internal view virtual override returns (string memory) {
+        return baseTokenURI;
+    }
+
+    function setBaseURI(string calldata baseURI) external onlyOwner {
+        baseTokenURI = baseURI;
+    }
+
+    //  Withdraw ETH
+    function withdraw() external onlyOwner {
+        uint256 balance = address(this).balance;
+        require(balance > 0, "Withdraw: Insufficient ETH");
+        (bool success, ) = payable(msg.sender).call{value: balance}("");
+        require(success, "Withdraw: Failed");
+    }
+}
--- metadata.py
+++ metadata.py
@@ -3,10 +3,13 @@ import pickle
 import json
 import random
 
-name = ""
+name = "Rich Rabbit NFT #"
 fee = "10%"
 description = "Crazy Rich Rabbit NFT @Montage Collection"
-count = 110
+image = "https://ipfs.io/ipfs/QmUneJUsj9A4Dt8JfrrR28BAnoDNTGqa2HG6BfXGezKfcZ/"
+count = 111
+
+like = ["CARROT", "RICE CAKE", "RADISH", "CUCUMBER", "BORCCOLI"]
 
 # Step 2
 for i in range(count):
@@ -14,18 +17,15 @@ for i in range(count):
 	filename = "metadata/" + str(i) + ".json";
 
 	#metadata
-	metadata = {'name': name, 'Royal fee': fee, 'description': description, 'attributes': []}
+	metadata = {'name': name + str(i), 'description': description, 'image': image + str(i) + '.jpg', 'attributes': []}
 	metadata["attributes"].append({"trait_type": "PASSIVE SKILL 1", "value": "DOUBLE SHOT"})
 	percent = random.randint(0,100)
 	if percent < 20:
 		metadata["attributes"].append({"trait_type": "PASSIVE SKILL 2", "value": "DRONE PILOT CERTIFICATE"})
 	metadata["attributes"].append({"trait_type": "LUCK", "value": random.randint(10,30)})
-	metadata["attributes"].append({"trait_type": "LIKE", "value": ["CARROT", "RICE CAKE", "RADISH", "CUCUMBER", "BORCCOLI"]})
+	metadata["attributes"].append({"trait_type": "LIKE", "value": like[random.randint(0,4)]})
 	metadata["attributes"].append({"trait_type": "COME FROM", "value": "MOON"})
 	metadata["attributes"].append({"trait_type": "SPECIAL FEATURE", "value": "MONTAGE"})
-	metadata["attributes"].append({"trait_type": "background", "value": "silver"})
-	metadata["attributes"].append({"trait_type": "background", "value": "silver"})
-	metadata["attributes"].append({"trait_type": "background", "value": "silver"})
 
 	# Serializing json
 	json_object = json.dumps(metadata, indent=4)
